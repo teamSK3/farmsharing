d0ccf95d7ff81d4cdad3cb5d702a32797e2749c5
RK3833 <wkc1wnr2@gmail.com>
2016-06-12 09:10:29 +0000

    fix error follow

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,7 @@
 class MembersController < ApplicationController
 	before_action :set_member, only: [:show, :edit, :update, :destroy]
 	# before_action :authenticate_member!
-  before_action :signed_in_member, only:[:index, :edit, :update, :destroy, :following, :followers]
+  # before_action :signed_in_member, only:[:index, :edit, :update, :destroy, :following, :followers]
 	def index
     @members = Member.order("id")
   	end
